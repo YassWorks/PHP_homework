ea5826d16942dee0449f12bebe181774aba0dba3
YassWorks <mohamedyassine.kallel@insat.ucar.tn>
2025-04-01 23:14:11 +0100

    ex2 final

--- Concepts de base/ex2/SessionManagerClass.php
+++ Concepts de base/ex2/SessionManagerClass.php
@@ -17,19 +17,30 @@ class SessionManager {
     public function addItemToSession($key, $value) {
         if (isset($_SESSION[$key])) {
             if ($this->debugMode) {
-                $log = "[INFO]: Key {$key} already exists in session, changing it to given value: $value";
+                $log = "[INFO]: Key \"$key\" already exists in session, changing it to given value: $value \n";
                 file_put_contents(SessionManager::LOG_FILE, $log, FILE_APPEND);
             }
         }
         $_SESSION[$key] = $value;
     }
 
-    public function getValueFromKey($key) {
+    public function deleteItemFromSession($key) {
+        if (isset($_SESSION[$key])) {
+            unset($_SESSION[$key]);
+        } else {
+            if ($this->debugMode) {
+                $log = "[WARNING]: Key \"$key\" not found \n";
+                file_put_contents(SessionManager::LOG_FILE, $log, FILE_APPEND);
+            }
+        }
+    }
+
+    public function getValueByKey($key) {
         if (isset($_SESSION[$key])) {
             return $_SESSION[$key];
         }
         if ($this->debugMode) {
-            $log = "[INFO]: Key {$key} not found";
+            $log = "[WARNING]: Key \"$key\" not found \n";
             file_put_contents(SessionManager::LOG_FILE, $log, FILE_APPEND);
         }
         return null;
@@ -39,7 +50,7 @@ class SessionManager {
         session_unset();
     }
 
-    public function __destruct() {
+    public function destroySession() {
         session_destroy();
     }
 }
--- Concepts de base/ex2/index.php
+++ Concepts de base/ex2/index.php
@@ -1,9 +1,57 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Session Manager Testing</title>
+</head>
+<body>
+    <h2>Centre de Shopping En ligne</h2>
+    <div>
+        <?php
+            require_once "SessionManagerClass.php";
 
-require_once "SessionManagerClass.php";
+            $sess = new SessionManager();
+            $msg = "";
+            $buttonPressed = false;
 
-$sess = new SessionManager();
-$sess->addItemToSession("id", 123);
-$sess->addItemToSession("id", 125);
+            if (isset($_POST['reset_button'])) {
+                $sess->destroySession();
+                $sess = new SessionManager();
+                $buttonPressed = true;
+            }
 
-?>
+            $SessionID = $sess->getValueByKey("SessionID");
+            if (!isset($SessionID)) {
+                $msg = "Bienvenu à notre plateforme.";
+                $sess->addItemToSession("SessionID", $_COOKIE['PHPSESSID']);
+                $sess->addItemToSession("#visits", 1);
+            } else {
+                $sess->addItemToSession("#visits", $sess->getValueByKey("#visits") + 1);
+                $n = $sess->getValueByKey("#visits");
+                $msg = "Merci pour votre fidélité, c’est votre {$n}éme visite.";
+            }
+
+            echo $msg;
+
+        ?>
+    </div>
+    <br>
+    <br>
+    <br>
+
+    <form action="" method="post">
+        <button type="submit" name="reset_button">réinitialiser la session</button>
+    </form>
+    <br>
+
+    <div>
+        <?php
+            if ($buttonPressed) {
+                echo "La session a été réinitialisée.";
+            }
+        ?>
+    </div>
+    
+</body>
+</html>
