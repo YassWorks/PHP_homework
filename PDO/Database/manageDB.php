5ef367dffadd4fb59c34f8607905be7d2d9c5952
Aymen Abid <aymen.abid20233@gmail.com>
2025-04-09 22:45:44 +0200

    updated ManageDB.php - manual insert to DB

--- PDO/Database/manageDB.php
+++ PDO/Database/manageDB.php
@@ -5,22 +5,22 @@ require_once "../Classes/UserClass.php";
 require_once "../Classes/StudentClass.php";
 require_once "../Classes/SectionClass.php";
 
-$usr1 = new User("john.doe", "aze123", "john.doe@fake.com", "Student");
-$stud1 = new Student(1, "john doe", "2000-02-05", 1, "./path");
+
+$stud1 = new Student(null, "john doe", "2000-02-05", 1, "./path", "john.doe", "john.doe@fake.com", "aze123");
 
 $usr2 = new User("jane.smith", "qwe456", "jane.smith@fake.com", "Admin");
 
-$usr3 = new User("michael.brown", "zxc789", "michael.brown@fake.com", "Student");
-$stud3 = new Student(3, "michael brown", "1999-01-01", 2, "./path");
+$stud3 = new Student(null, "michael brown", "1999-01-01", 2, "./path", "michael.brown", "michael.brown@fake.com", "zxc789");
 
-$usr5 = new User("david.wilson", "fgh654", "david.wilson@fake.com", "Student");
-$stud5 = new Student(4, "david wilson", '2002-08-08', 1, "./path");
+$usr4 = new User("aymen.sellaouti", "sdf123", "aymen.sellaouti@fake.com", "Admin");
+
+$stud5 = new Student(null, "david wilson", "2002-08-08", 1, "./path", "david.wilson", "david.wilson@fake.com", "fgh654");
 
 $gl = new Sections("GL", "Genie Logiciel");
 $rt = new Sections("RT", "Resaux");
 
 $secs = [$gl, $rt];
-$usrs = [$usr1, $usr2, $usr3, $usr5];
+$usrs = [$usr2, $usr4];
 $studs = [$stud1, $stud3, $stud5];
 
 foreach($secs as $sec) {
